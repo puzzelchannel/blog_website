8d5fbfbf9c0d3ffc8e7780113523edfb359e9511
puzzelchannel <puzzel.channel@gmail.com>
2022-12-06 19:20:50 +0330

    fix more bug

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,12 @@
-from django.shortcuts import render, get_object_or_404
 import os
+
+from django.shortcuts import render, get_object_or_404
+
 from .models import Post
 
 os.system('clear')
+
+
 # Create your views here.
 def post_list(request):
     posts = Post.objects.filter(status='pub')
