9b8a008ae1521f493676a19859584ecfa58f3c46
puzzelchannel <puzzel.channel@gmail.com>
2022-12-07 13:58:10 +0330

    add CRUD to blog phase one

--- blog/urls.py
+++ blog/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from .views import post_list, post_detail
+from .views import post_list, post_detail, create_post
 
 urlpatterns = [
     path('', post_list, name='posts_list'),
     path('<int:id>/', post_detail, name='posts_detail'),
+    path('add_post/', create_post, name='post_create'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,7 @@
 import os
-
 from django.shortcuts import render, get_object_or_404
-
+from django.shortcuts import redirect
+from .forms import NewPostForm
 from .models import Post
 
 os.system('clear')
@@ -22,3 +22,19 @@ def post_detail(request, id):
         'posts_detail': post_detail,
     }
     return render(request, template_name='blog/post_detail.html', context=context)
+
+
+def create_post(request):
+    if request.method == 'POST':
+        form = NewPostForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('posts_list')
+
+    else:
+        form = NewPostForm()
+
+    context = {
+        'create_post_form': form,
+    }
+    return render(request, template_name='blog/post_create.html', context=context)
